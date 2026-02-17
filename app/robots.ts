bbb6fe44ebc621faabea30d3a9a6d8966f8c54be
Avishek Majumder <avishekalex15@gmail.com>
2026-02-17 10:00:55 +0800

    fix: explicit crawler rules in robots.ts to resolve SEO issue

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,17 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-            disallow: ["/private/", "/admin/", "/api/", "/google-apps-script/", "/results/"],
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: ["/private/", "/admin/", "/api/", "/google-apps-script/", "/results/", "/results/*"],
+            },
+            {
+                userAgent: "GPTBot",
+                allow: "/",
+            }
+        ],
         sitemap: "https://www.invaritech.ai/sitemap.xml",
         host: "https://www.invaritech.ai",
     };
